858f8df736daa676a21417e4d8422e036b7e03ab
Mark Wilkinson <56736714+mewtonium@users.noreply.github.com>
2025-07-20 14:07:17 +0100

    Added base rate limit for API routes

--- routes/api/main.php
+++ routes/api/main.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')
+Route::middleware(['auth:sanctum', 'throttle:60,1'])
     ->as('api.')
     ->group(function (): void {
         require __DIR__ . '/v1.php';
